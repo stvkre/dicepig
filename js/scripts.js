d80441bc76177d0f54bfb5a4c88ce9d78cbcb9e0
stvkre <kiariemburuh@gmail.com>
2017-03-03 18:04:10 +0300

    add all files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function run() {
+  var die1 = document.getElementById("die1");
+  var span1 = document.getElementById("span1");
+
+  var core = Math.floor(Math.random() * 6) + 1;
+
+  die1.innerHTML = core;
+  span1.innerHTML = core;
+
+
+
+}
+
+$(document).ready(function() {
+  $("button#slide").click(function() {
+    $(".jumbotron").hide(1000);
+    $("#pitch").slideDown();
+  });
+});
